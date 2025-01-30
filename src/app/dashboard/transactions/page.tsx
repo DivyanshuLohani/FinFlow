b53d32e0328215ed74e0752f821d913c8a7d8f38
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2025-01-30 12:43:11 +0530

    remove unused input

--- src/app/dashboard/transactions/page.tsx
+++ src/app/dashboard/transactions/page.tsx
@@ -1,6 +1,5 @@
 import { getCategories } from "@/lib/transaction/service";
 import TransactionList from "./components/TransactionList";
-import AddIncomeDialog from "../components/AddIncomeDialog";
 
 export default async function TransactionsPage() {
   const categories = await getCategories();
