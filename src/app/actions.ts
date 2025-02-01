9a75f39b3afcc12967b36fed8bff977e88460ac4
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2025-02-01 14:31:08 +0530

    fix data fetching

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,16 +1,31 @@
 "use server";
 
 import { prisma } from "@/lib/database/prisma";
-import { TransactionType } from "@prisma/client";
 import { unstable_cache } from "next/cache";
 
 const getHomePageData = unstable_cache(
   async () => {
-    const [expenses, income, users] = await prisma.$transaction([
-      prisma.transaction.count({ where: { type: TransactionType.EXPENSE } }),
-      prisma.transaction.count({ where: { type: TransactionType.EXPENSE } }),
+    const [transactions, users] = await prisma.$transaction([
+      prisma.transaction.groupBy({
+        by: ["type"],
+        _sum: {
+          amount: true,
+        },
+        orderBy: { type: "asc" },
+      }),
       prisma.user.count(),
     ]);
+
+    const expenses = Math.min(
+      10000000,
+      transactions.find((transaction) => transaction.type === "EXPENSE")?._sum
+        ?.amount || 0
+    );
+    const income = Math.min(
+      10000000,
+      transactions.find((transaction) => transaction.type === "INCOME")?._sum
+        ?.amount || 0
+    );
 
     return { expenses, income, users };
   },
