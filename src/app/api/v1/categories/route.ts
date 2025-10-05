318082d8c1f5d314837cf368a936e16558a1ab7e
Divyanshu Lohani <DivyanshuLohani@users.noreply.github.com>
2025-10-05 16:07:14 +0530

    refactor: update CategoryCard to display formatted income and expenses

--- src/app/api/v1/categories/route.ts
+++ src/app/api/v1/categories/route.ts
@@ -1,9 +1,34 @@
+import { prisma } from "@/lib/database/prisma";
 import { createCategory, getCategories } from "@/lib/transaction/service";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   const categories = await getCategories();
-  return NextResponse.json(categories);
+  const returnCategories = [];
+  for (const category of categories) {
+    const transactions = await prisma.transaction.findMany({
+      where: { categoryId: category.id },
+    });
+
+    const totalIncome = transactions.reduce(
+      (sum, t) => sum + (t.type === "INCOME" ? t.amount : 0),
+      0
+    );
+
+    const totalExpenses = transactions.reduce(
+      (sum, t) => sum + (t.type === "EXPENSE" ? t.amount : 0),
+      0
+    );
+
+    returnCategories.push({
+      id: category.id,
+      name: category.name,
+      special: category.special,
+      totalIncome,
+      totalExpenses,
+    });
+  }
+  return NextResponse.json(returnCategories);
 }
 
 export async function POST(req: Request) {
