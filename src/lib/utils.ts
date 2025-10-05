ccad114f8bfab1f0e1305e2b33157ad0d51c6242
Divyanshu Lohani <DivyanshuLohani@users.noreply.github.com>
2025-10-05 19:17:32 +0530

    feat: enhance TransactionRow with category color badge and text color adjustment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -25,6 +25,23 @@ export function formatDate(
   }).format(date);
 }
 
+export function getTextColorFromBg(background: string) {
+  const color =
+    background.charAt(0) === "#" ? background.substring(1, 7) : background;
+  const r = parseInt(color.substring(0, 2), 16); // hexToR
+  const g = parseInt(color.substring(2, 4), 16); // hexToG
+  const b = parseInt(color.substring(4, 6), 16); // hexToB
+  const uicolors = [r / 255, g / 255, b / 255];
+  const c = uicolors.map((col) => {
+    if (col <= 0.03928) {
+      return col / 12.92;
+    }
+    return Math.pow((col + 0.055) / 1.055, 2.4);
+  });
+  const L = 0.2126 * c[0] + 0.7152 * c[1] + 0.0722 * c[2];
+  return L > 0.179 ? "#000000" : "#FFFFFF";
+}
+
 /**
  * Formats a number into currenget cy format based on the user's locale.
  *
