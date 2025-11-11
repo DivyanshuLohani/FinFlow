3477a9664832454a0862d746ab3bdb9a30dcedc8
Divyanshu Lohani <DivyanshuLohani@users.noreply.github.com>
2025-11-11 10:34:34 +0530

    Implement user registration with email normalization and JWT token generation

--- src/app/api/mobile/register/route.ts
+++ src/app/api/mobile/register/route.ts
@@ -1,5 +1,43 @@
-import { POST as v1POST } from "@/app/api/v1/users/route";
+import { EMAIL_VERIFICATION_DISABLED } from "@/lib/constants";
+import { createUser } from "@/lib/user/service";
 import { NextRequest } from "next/server";
-export const POST = async (req: NextRequest) => {
-  return v1POST(req);
+import jwt from "jsonwebtoken";
+
+export const POST = async (request: NextRequest) => {
+  let user = await request.json();
+
+  try {
+    user = {
+      ...user,
+      email: user.email.toLowerCase(),
+    };
+
+    user = await createUser(user);
+    if (!EMAIL_VERIFICATION_DISABLED) {
+      // TODO: Send Verification Email
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, {
+      expiresIn: "365d",
+    });
+
+    return Response.json({ user, token });
+  } catch (e: any) {
+    if (e.message === "User with this email already exists") {
+      return Response.json(
+        {
+          error: "User with this email already exists",
+          errorCode: e.code,
+        },
+        { status: e.statusCode }
+      );
+    }
+    return Response.json(
+      {
+        error: e.message,
+        errorCode: e.code,
+      },
+      { status: 500 }
+    );
+  }
 };
